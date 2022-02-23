c6b59e9888901471aaa2d76a8a70cb953516b4f4
Marek <m.arendt04@gmail.com>
2022-02-23 19:44:00 +0100

    ModelAdmin polishing.

--- apps/teams/wagtail_hooks.py
+++ apps/teams/wagtail_hooks.py
@@ -6,10 +6,9 @@ from .models import Team
 class TeamModelAdmin(ModelAdmin):
     model = Team
     menu_label = 'Teams'
-    menu_icon = 'Group'
+    menu_icon = 'group'
     menu_order = 200
-    list_display = ('name', 'image')
-    list_filter = ('name',)
+    list_display = ('name',)
     search_fields = 'name'
 
 
--- league_manager/settings/base.py
+++ league_manager/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wagtail.contrib.forms',
+    'wagtail.contrib.modeladmin',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
     'wagtail.sites',
