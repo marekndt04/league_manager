982d3dde683a3d168259337f7da9e411e64052f9
Marek <m.arendt04@gmail.com>
2022-03-03 19:54:59 +0100

    Modified tests due to changes in urls.

--- apps/seasons/tests/test_views.py
+++ apps/seasons/tests/test_views.py
@@ -10,17 +10,21 @@ from apps.seasons.models import SeasonPage
 class TestSeasonView(TestCase):
     def setUp(self):
         self.root_page = Page.objects.get(slug='root')
+        self.season_1 = SeasonPage(title='New Season', slug='new-season')
+        self.season_2 = SeasonPage(title='Newest Season', slug='newest-season')
+        self.root_page.add_child(instance=self.season_1)
+        self.root_page.add_child(instance=self.season_2)
+        self.season_1.save()
+        self.season_2.save()
 
-    def test_view_returns_http_ok_for_existing_object(self):
-        new_season = SeasonPage(title='New Season', slug='new-season')
-        self.root_page.add_child(instance=new_season)
-        new_season.save()
-
-        self.url = reverse('season-view', args=[new_season.id])
+    def test_view_returns_http_ok_for_existing_season_object(self):
+        self.url = reverse('season-view')
         response = self.client.get(self.url)
+
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
 
-    def test_view_returns_http_not_found_for_unexisting_object(self):
-        self.url = reverse('season-view', args=[666])
+    def test_view_returns_last_season_in_context(self):
+        self.url = reverse('season-view')
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND)
+
+        self.assertEqual(response.context['object_list'].title, self.season_2.title)
