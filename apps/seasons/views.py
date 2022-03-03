9b752091efe5a9c6fc9c8ed7eb75110585b77628
Marek <m.arendt04@gmail.com>
2022-03-03 19:56:32 +0100

    View fixed.

--- apps/seasons/views.py
+++ apps/seasons/views.py
@@ -6,7 +6,7 @@ from apps.teams.models import Team
 
 class SeasonView(ListView):
     template_name = 'seasons/seasonpage_detail.html'
-    queryset = SeasonPage.objects.first()
+    queryset = SeasonPage.objects.last()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
